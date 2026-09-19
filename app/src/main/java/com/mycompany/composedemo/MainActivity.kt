8b15d75ad25a449fa6c56e9efd8b5107dba7bed0
mondstatroza-jpg <mondstatroza@gmail.com>
2026-09-19 10:08:26 +0700

    Compose

--- app/src/main/java/com/mycompany/composedemo/MainActivity.kt
+++ app/src/main/java/com/mycompany/composedemo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ComposeDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Compose",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
